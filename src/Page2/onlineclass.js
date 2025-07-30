037e160bde3a5d5c162dc91c07876026370c7937
joann <joanngaas@gmail.com>
2025-07-30 07:38:46 -0700

    m

--- src/Page2/onlineclass.js
+++ src/Page2/onlineclass.js
@@ -6,7 +6,7 @@ const classData = [
   
     {
         id: 1,
-        title: 'Fundamentals of Programming',
+        title:'Major Courses -Payao Mobile Class 1st Year',
         instructor: 'Jo Ann Ga-as',
         description: 'Introduction to basic coding concepts like variables, control structures, data types, and problem-solving using programming languages such as C, Python, or Java.',
         yearlevel: '1st year',
@@ -16,53 +16,25 @@ const classData = [
     },
     {
         id: 2,
-        title: 'Data Communication',
+        title:'Major Courses -Payao Mobile Class  2nd Year',
         instructor: 'Jo Ann Ga-as',
-        description: 'Covers how data is transmitted between devices over networks, including transmission media, protocols, bandwidth, and error handling.',
+        description: 'Introduction to basic coding concepts like variables, control structures, data types, and problem-solving using programming languages such as C, Python, or Java.',
         yearlevel: '1st year',
-        time: 'Mon 2pm-3pm',
-        link: 'https://meet.jit.si/MAJORCOURSE-PAYAOmeet.jit.si',
+        time: 'Sunday 7PM - 8pm',
+        link: 'https://meet.jit.si/MajorCourses-PayaoMobileClass2ndYear',
 
     },
-    {
+     {
         id: 3,
-        title: 'Introduction to Computing',
-        instructor: 'Jo Ann',
-        description: 'Overview of computer systems, components, software, and basic IT concepts including the role of computers in society.',
-        yearlevel: '2nd year',
-        time: 'Tue 6PM-7pm',
-        link: 'https://meet.jit.si/MAJORCOURSE-PAYAOmeet.jit.si',
-
-    },
-    {
-        id: 4,
-        title: 'Network Security & Network Administration',
+        title:'Major Courses -Siparoc Mobile Class  1st Year',
         instructor: 'Jo Ann Ga-as',
-        description: 'Study of methods to protect networks and data from cyber threats, including encryption, firewalls, antivirus, and security policies.',
-        yearlevel: '2nd year',
-        time: 'Tue 6pm-7pm',
-        link: 'https://meet.jit.si/MAJORCOURSE-PAYAOmeet.jit.si',
-        
-    },
-    {
-        id: 5,
-        title: 'Data Structures and Algorithms',
-        instructor: 'Jo Ann Ga-as',
-        description: 'Explores how data is organized and manipulated using structures like arrays, stacks, queues, and algorithms for searching, sorting, and optimization.',
-        yearlevel: '2nd year',
-        time: 'Tue 6pm-7pm',
-        link: 'https://meet.jit.si/MAJORCOURSE-PAYAOmeet.jit.si',
-        
-    },
-    {
-        id: 6,
-        title: 'Teaching of Jesus',
-        instructor: 'Ping Cernal',
-        description: 'Teachings of Jesus is a values education subject that introduces students to the life, message, and moral teachings of Jesus Christ based on the Gospels.',
+        description: 'Introduction to basic coding concepts like variables, control structures, data types, and problem-solving using programming languages such as C, Python, or Java.',
         yearlevel: '1st year',
-        time: 'Tue 8-9pm',
-        link: 'https://meet.jit.si/ValuesTeachinofJesus',
+        time: 'Mon 1PM - 2pm',
+        link: 'https://meet.jit.si/MajorCourses-SiparocMobileClass1stYear',
     },
+ 
+ 
 
 ];
 
